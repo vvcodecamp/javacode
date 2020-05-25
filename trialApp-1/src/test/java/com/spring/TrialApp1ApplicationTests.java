a3c1a4c9ba4cec21756692b013fb5beac758829d
Jeya <Jeya@Vadi>
2020-05-25 11:25:42 +0530

    adaf

---
+++ trialApp-1/src/test/java/com/spring/TrialApp1ApplicationTests.java
@@ -0,0 +1,14 @@
+package com.spring;
+//test
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrialApp1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
