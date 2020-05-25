456cd03dcace71e5c34687a906ce5b855425644e
Jeya <Jeya@Vadi>
2020-05-25 11:27:47 +0530

    retest

--- trialApp-1/src/main/java/com/spring/TrialApp1Application.java
+++ trialApp-1/src/main/java/com/spring/TrialApp1Application.java
@@ -2,7 +2,7 @@ package com.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//retest
 @SpringBootApplication
 public class TrialApp1Application {
 
